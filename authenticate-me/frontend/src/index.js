1c6b003cb8a923983830e2aa3a244bb86c7337fc
AugustinoE <elisaia55@yahoo.com>
2022-05-26 13:03:21 -0700

    phase0pt2

--- authenticate-me/frontend/src/index.js
+++ authenticate-me/frontend/src/index.js
@@ -5,13 +5,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
   window.store = store;
 }
+
 
 function Root() {
   return (

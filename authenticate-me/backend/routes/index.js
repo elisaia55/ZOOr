9f0adbe5f6d41094b4e6de85de142885725ec283
AugustinoE <elisaia55@yahoo.com>
2022-05-26 12:03:19 -0700

    finishedPart1

--- authenticate-me/backend/routes/index.js
+++ authenticate-me/backend/routes/index.js
@@ -3,9 +3,5 @@ const router = express.Router();
 const apiRouter = require('./api')
 router.use('/api', apiRouter)
 
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
 
 module.exports = router;

d15c65727145c6a8b8f73cc91ec34cb9fa523d0e
AugustinoE <elisaia55@yahoo.com>
2022-06-04 14:48:56 -0700

    recaptchaLogin

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import './SignupForm.css'
+import logo from '../../images/ZOOr LOG FINAL.png'
+import ReCAPTCHA from "react-google-recaptcha"
 
 function SignupFormPage() {
     const dispatch = useDispatch();
@@ -14,6 +16,11 @@ function SignupFormPage() {
     const [errors, setErrors] = useState([]);
 
     if (sessionUser) return <Redirect to="/" />;
+
+    function onCaptchaChange(value) {
+        console.log("Captcha value", value)
+    }
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -29,48 +36,56 @@ function SignupFormPage() {
     };
 
     return (
-        <form onSubmit={ handleSubmit }>
-            <ul>
-                { errors.map((error, idx) => <li key={ idx }>{ error }</li>) }
-            </ul>
-            <label>
-                Email
-                <input
-                    type="text"
-                    value={ email }
-                    onChange={ (e) => setEmail(e.target.value) }
-                    required
-                />
-            </label>
-            <label>
-                Username
-                <input
-                    type="text"
-                    value={ username }
-                    onChange={ (e) => setUsername(e.target.value) }
-                    required
-                />
-            </label>
-            <label>
-                Password
-                <input
-                    type="password"
-                    value={ password }
-                    onChange={ (e) => setPassword(e.target.value) }
-                    required
-                />
-            </label>
-            <label>
-                Confirm Password
-                <input
-                    type="password"
-                    value={ confirmPassword }
-                    onChange={ (e) => setConfirmPassword(e.target.value) }
-                    required
-                />
-            </label>
-            <button type="submit">Sign Up</button>
-        </form>
+        <div className="sign-up-container">
+            <img src={ logo }></img>
+            <p className="sign-up-header">Sign up for ZOOr</p>
+            <ReCAPTCHA sitekey="6Lc350QgAAAAAHOQ6-O2tDuezdnqv0cC_x-qzrNQ"
+                onChange={ onCaptchaChange } />
+
+
+            <form onSubmit={ handleSubmit }>
+                <ul>
+                    { errors.map((error, idx) => <li key={ idx }>{ error }</li>) }
+                </ul>
+                <label>
+                    Email
+                    <input
+                        type="text"
+                        value={ email }
+                        onChange={ (e) => setEmail(e.target.value) }
+                        required
+                    />
+                </label>
+                <label>
+                    Username
+                    <input
+                        type="text"
+                        value={ username }
+                        onChange={ (e) => setUsername(e.target.value) }
+                        required
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        value={ password }
+                        onChange={ (e) => setPassword(e.target.value) }
+                        required
+                    />
+                </label>
+                <label>
+                    Confirm Password
+                    <input
+                        type="password"
+                        value={ confirmPassword }
+                        onChange={ (e) => setConfirmPassword(e.target.value) }
+                        required
+                    />
+                </label>
+                <button type="submit">Sign Up</button>
+            </form>
+        </div>
     );
 }
 
